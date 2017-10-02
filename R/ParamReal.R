ce8933212e8c9a50a408865108e30daf16608526
Jakob Richter <code@jakob-r.de>
2017-10-02 12:24:59 +0200

    ParamReal

---
+++ R/ParamReal.R
@@ -0,0 +1,20 @@
+ParamInt = R6Class(
+  "ParamReal",
+  inherit = ParamSimple,
+  public = list(
+    lower = NULL,
+    upper = NULL,
+    special.val = NULL,
+    initialize = function(id = NULL, lower, upper) {
+      check = function(x, na.ok = FALSE, null.ok = FALSE) checkInt(x, lower = lower, upper = upper, na.ok = na.ok, null.ok = null.ok)
+      super$initialize(id = id, type = "integer", check = check)
+      self$lower = lower
+      self$upper = upper
+    },
+    sample = function() {
+      as.integer(round(runif(1, min = self$lower-0.5, max = self$upper+0.5)))
+    }
+  ),
+  private = list(
+  )
+)
