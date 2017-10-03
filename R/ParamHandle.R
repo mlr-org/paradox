51eceda85a12c695162dfcf25c86efb9e00b7974
smilesun <smilesun.east@gmail.com>
2017-10-03 13:39:31 +0200

    tree demo works

--- R/ParamHandle.R
+++ R/ParamHandle.R
@@ -1,6 +1,7 @@
 ParamHandle = R6Class("ParamHandle",
   inherit = ParamBase, # FIXME: Are we sure? Yes!
   public = list(
+    id = NULL,
     node = NULL,
     val = NULL,
     root = NULL,
@@ -8,7 +9,8 @@ ParamHandle = R6Class("ParamHandle",
     flatval = NULL,
     mand.children = NULL,
     cond.children = NULL,
-    initialize = function(node = NULL, val = NULL, depend = NULL) {
+    initialize = function(id = NULL, node = NULL, val = NULL, depend = NULL) {
+      self$id = id
       self$node = node
       self$val = val
       self$depend = depend
@@ -32,20 +34,29 @@ ParamHandle = R6Class("ParamHandle",
       self$depend = pnode
     },
     sampleCurrentNode = function() {
-      self$node$sample()
+      print(self$val)
+      #self$node$sample()
     },
     sampleMandChildChain = function() {
-      for(handle in self$mand.children) handle$node$sample()
+      #for(handle in self$mand.children) handle$node$sample()
+      if(length(self$mand.children) == 0) return(NULL)
+      for(name in names(self$mand.children)) {
+        handle = self$mand.children[[name]]
+        handle$sample()
+      }
     },
-    sampleCondChildChain = function(expr) {
-      for(handle in self$cond.children) {
-        if(expr(self$node)) handle$node$sample()
+    sampleCondChildChain = function(expr = function(x) {return(TRUE)}) {
+      if(length(self$cond.children) == 0) return(NULL)
+      for(name in names(self$cond.children)) {
+        handle = self$cond.children[[name]]
+        #if(expr(self$node)) handle$node$sample()
+        if(expr(self$node)) handle$sample()
       }
     },
     sample = function() {
-      sampleCurrentNode()
-      sampleMandChildChain()
-      sampleCondChildChain()
+      self$sampleCurrentNode()
+      self$sampleMandChildChain()
+      self$sampleCondChildChain()
     }
   )
 )
--- tests/testthat/test_ParamHandle.R
+++ tests/testthat/test_ParamHandle.R
@@ -1,6 +1,7 @@
 context("ParamHandle")
 
 test_that("test if ParamHandle constructor works", {
-  ps = ParamHandle$new(val = 3)
-  ps$addMandChild(ParamHandle$new())$addMandChild(ParamHandle$new())
+  ps = ParamHandle$new(id = "SVM", val = "SVM")
+  ps$addMandChild(ParamHandle$new(id = "kernel", val = "rbf"))$addMandChild(ParamHandle$new(id = "gamma", val = 0.6))
+  ps$sample()
 })
