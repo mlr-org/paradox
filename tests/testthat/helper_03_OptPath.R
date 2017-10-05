3dae188fbd773ea5a3fe8e8eb051cffaf6b41264
Jakob Richter <code@jakob-r.de>
2017-10-05 16:32:55 +0200

    disable outdated tests

--- tests/testthat/helper_03_OptPath.R
+++ tests/testthat/helper_03_OptPath.R
@@ -2,22 +2,22 @@
 
 th.opt.path.full = OptPath$new(par.set = th.paramset.flat.full)
 
-for (i in 1:10) {
-  x = th.opt.path.full$par.set$sample(1)
-  th.opt.path.full$add(
-    x = x,
-    y = i, 
-    message = sprintf("message: %i", i),
-    error = sprintf("error: %i", i),
-    exec.time = i,
-    extra = list(th.ex1 = i, th.ex2 = "th.ex2.string")
-  )
-}
+# for (i in 1:10) {
+#   x = th.opt.path.full$par.set$sample(1)
+#   th.opt.path.full$add(
+#     x = x,
+#     y = i, 
+#     message = sprintf("message: %i", i),
+#     error = sprintf("error: %i", i),
+#     exec.time = i,
+#     extra = list(th.ex1 = i, th.ex2 = "th.ex2.string")
+#   )
+# }
 
 # Multi-objective opt.path
 
-th.opt.path.multiobjective = OptPath$new(par.set = th.paramset.flat.full, y.names = c('y1.min', 'y2.max'), minimize = c(TRUE, FALSE))
-for (i in 1:10) {
-  x = th.opt.path.multiobjective$par.set$sample(1)
-  th.opt.path.multiobjective$add(x = x, y = c(y2.max = i, y1.min = 10-i))
-}
+# th.opt.path.multiobjective = OptPath$new(par.set = th.paramset.flat.full, y.names = c('y1.min', 'y2.max'), minimize = c(TRUE, FALSE))
+# for (i in 1:10) {
+#   x = th.opt.path.multiobjective$par.set$sample(1)
+#   th.opt.path.multiobjective$add(x = x, y = c(y2.max = i, y1.min = 10-i))
+# }
