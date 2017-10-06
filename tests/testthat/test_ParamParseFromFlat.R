6f0e24555a249959fe9649f046965ac0d7ff0173
smilesun <smilesun.east@gmail.com>
2017-10-06 12:41:22 +0200

    parse from flat true

--- tests/testthat/test_ParamParseFromFlat.R
+++ tests/testthat/test_ParamParseFromFlat.R
@@ -1,6 +1,7 @@
 context("ParseFromFlat")
 
 test_that("test if Param parse from flat works", {
+  # in this simple example, the node is not defined, so the sample function here does not work
   ps = ParamHandle$new(id = "Root", val = NULL)
   input = list(
     model = list(id = "model", val = "svm"),
@@ -16,12 +17,12 @@ test_that("test if Param parse from flat works", {
 test_that("test if Param parse from flat works with sample", {
   ps = ParamHandle$new(id = "Root", val = NULL)
   input = list(
-    model = list(id = "model", val = "svm"),
-    c = list(id = "C", val = 3, depend = list(id = "model", val = "svm")),
-    kernel = list(id = "kernel", val = "rbf", depend = list(id = "model", val = "svm")),
-    gamma = list(id = "gamma", val = "0.3", depend = list(id = "kernel", val = "rbf"))
-  )
+    model = list(id = "model", ParamFactor$new(id = "model", values = c("SVM", "RF")), val = "svm"),
+    c = list(id = "C", node = ParamReal$new(id = "C", lower = 0, upper = 100), val = 3, depend = list(id = "model", val = "svm")),
+    kernel = list(id = "kernel", ParamHandle$new(id = "kernel", node = ParamFactor$new(id = "kernel", values = c("rbf", "poly")), val = "rbf", depend = list(id = "model", val = "svm"))),
+    gamma = list(id = "gamma", ParamReal$new(id = "gamma", lower = 0, upper = 100), val = 0.3, depend = list(id = "kernel", val = "rbf"))
+ ) 
   ps$visitor$parseFlat(input)
-  ps$toStringVal()
-  ps$sample()
+  #ps$toStringVal()
+  #ps$sample()
 })
