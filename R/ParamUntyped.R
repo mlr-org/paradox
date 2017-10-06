d4e9d2b4469e2fad0d84c15046853bb9ca70a967
Jakob Richter <code@jakob-r.de>
2017-10-06 12:44:38 +0200

    Untyped Param

---
+++ R/ParamUntyped.R
@@ -0,0 +1,24 @@
+ParamUntyped = R6Class(
+  "ParamUntyped",
+  inherit = ParamSimple,
+  public = list(
+
+    # member variables
+
+    # constructor
+    initialize = function(id, default = NULL, tags = NULL) {
+      check = function(x, na.ok = FALSE, null.ok = FALSE) return(TRUE)
+
+      # construct super class
+      super$initialize(id = id, type = "list", check = check, default = default, tags = tags)
+    },
+
+    # public methods
+    sampleVector = function(n = 1L) {
+      stop("Untped Param can not be sampled")
+    },
+    denormVector = function(x) {
+      stop("Untyped Param can not be denormed")
+    }
+  )
+)
