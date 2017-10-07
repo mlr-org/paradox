9a933e2c06029dc40dc8c0355c15a032d11348a0
Jakob Richter <code@jakob-r.de>
2017-10-07 11:41:23 +0200

    cleanup/tree

--- tests/testthat/test_ParamInt.R
+++ tests/testthat/test_ParamInt.R
@@ -2,7 +2,7 @@ context("ParamInt")
 
 test_that("test if ParamInt constructor works", {
   pint = ParamInt$new(id = "test", lower = 1L, upper = 10L)
-  print(pint$sample())
+  expect_data_table(pint$sample())
 })
 
 
