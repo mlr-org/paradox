34df059ec0730ef716221b199ea442c8c166f582
Jakob Richter <code@jakob-r.de>
2017-10-11 17:14:54 +0200

    deploy update

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -3,10 +3,10 @@
 R --no-save <<< 'devtools::install(); pkgdown::build_site()'
 git checkout master
 export TRAVIS_COMMIT_MSG="$(git log --format=%B --no-merges -n 1)"
-git config user.name "$GN"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config --global user.name "Travis CI"
+git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 git config credential.helper "store --file=.git/credentials"
-echo "https://$GT:@github.com" >> .git/credentials
+echo "https://${GH_TOKEN}:@github.com" >> .git/credentials
 git config push.default matching
 git add --force man/*
 git add --force README.md
