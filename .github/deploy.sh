262efbbd222d5ef03b7948dd1211fce10cf2fcfc
Jakob Richter <code@jakob-r.de>
2017-10-11 12:37:03 +0200

    checking and building

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+R --no-save <<< 'pkgdown::build_site()'
 git checkout master
 "export TRAVIS_COMMIT_MSG=\"$(git log --format=%B --no-merges -n 1)\""
 git config user.name $GN
@@ -9,5 +10,7 @@ echo "https://$GT:@github.com" >> .git/credentials
 
 git config push.default matching
 git add --force man/*
-git commit man DESCRIPTION NAMESPACE -m "update auto-generated documentation [ci skip]" || true
+git add --force README.md
+git add --force docs/*
+git commit man DESCRIPTION NAMESPACE README.md docs -m "update auto-generated documentation [ci skip]" || true
 git push
