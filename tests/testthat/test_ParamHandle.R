103cbf6c0be8f880e62aee037c298985ed639bf0
Jakob Richter <code@jakob-r.de>
2017-10-13 13:00:58 +0200

    renaming

--- tests/testthat/test_ParamHandle.R
+++ tests/testthat/test_ParamHandle.R
@@ -12,9 +12,9 @@ test_that("test if ParamHandle constructor works", {
 
 
 test_that("test if ParamHandle constructor works with SimpleParamNode", {
-  ps = ParamHandle$new(id = "Model", node = ParamFactor$new(id = "Model", values = c("SVM", "RF")), val = "SVM")
+  ps = ParamHandle$new(id = "Model", node = ParamCategorical$new(id = "Model", values = c("SVM", "RF")), val = "SVM")
   ps$addCondChild(ParamHandle$new(id = "C", node = ParamReal$new(id = "C", lower = 0, upper = 100), depend = list(id = "Model", val = "SVM"),val = 0.3))
-  kernel = ps$addCondChild(ParamHandle$new(id = "kernel", node = ParamFactor$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "Model", val = "SVM") , val = "rbf"))
+  kernel = ps$addCondChild(ParamHandle$new(id = "kernel", node = ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "Model", val = "SVM") , val = "rbf"))
   kernel$addCondChild(ParamHandle$new(id = "gamma", ParamReal$new(id = "gamma", lower = 0, upper = 100), val = 0.6, depend = list(id = "kernel", val = "rbf")))
   kernel$addCondChild(ParamHandle$new(id = "n", node = ParamInt$new(id = "n", lower = 1L, upper = 10L), val = 3L, depend = list(id = "kernel", val = "poly")))
   ps$visitor$toFlat()
@@ -27,9 +27,9 @@ test_that("test if ParamHandle constructor works with SimpleParamNode", {
 
 
 test_that("test if ParamHandle toStringVal behaves normal without value", {
-  ps = ParamHandle$new(id = "Model", node = ParamFactor$new(id = "Model", values = c("SVM", "RF")), val = "SVM")
+  ps = ParamHandle$new(id = "Model", node = ParamCategorical$new(id = "Model", values = c("SVM", "RF")), val = "SVM")
   ps$addCondChild(ParamHandle$new(id = "C", node = ParamReal$new(id = "C", lower = 0, upper = 100), depend = list(id = "Model", val = "SVM")))
-  kernel = ps$addCondChild(ParamHandle$new(id = "kernel", node = ParamFactor$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "Model", val = "SVM")))
+  kernel = ps$addCondChild(ParamHandle$new(id = "kernel", node = ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "Model", val = "SVM")))
   kernel$addCondChild(ParamHandle$new(id = "gamma", ParamReal$new(id = "gamma", lower = 0, upper = 100), depend = list(id = "kernel", val = "rbf")))
   kernel$addCondChild(ParamHandle$new(id = "n", node = ParamInt$new(id = "n", lower = 1L, upper = 10L), depend = list(id = "kernel", val = "poly")))
   ps$toStringVal()
--- tests/testthat/test_ParamParseFromFlat.R
+++ tests/testthat/test_ParamParseFromFlat.R
@@ -17,9 +17,9 @@ test_that("test if Param parse from flat works", {
 test_that("test if Param parse from flat works with sample", {
   ps = ParamHandle$new(id = "Root", val = NULL)
   input = list(
-    model = list(id = "model", ParamFactor$new(id = "model", values = c("SVM", "RF")), val = "svm"),
+    model = list(id = "model", ParamCategorical$new(id = "model", values = c("SVM", "RF")), val = "svm"),
     c = list(id = "C", node = ParamReal$new(id = "C", lower = 0, upper = 100), val = 3, depend = list(id = "model", val = "svm")),
-    kernel = list(id = "kernel", node = ParamFactor$new(id = "kernel", values = c("rbf", "poly")), val = "rbf", depend = list(id = "model", val = "svm")),
+    kernel = list(id = "kernel", node = ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), val = "rbf", depend = list(id = "model", val = "svm")),
     gamma = list(id = "gamma", ParamReal$new(id = "gamma", lower = 0, upper = 100), val = 0.3, depend = list(id = "kernel", val = "rbf"))
  ) 
   ps$visitor$parseFlat(input)
