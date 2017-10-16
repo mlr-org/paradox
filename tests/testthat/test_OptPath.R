c54877cf8f500555a58bfb8a9c5fa082f8b1b26d
Jakob Richter <code@jakob-r.de>
2017-10-16 17:24:42 +0200

    activate optpath tests

--- tests/testthat/test_OptPath.R
+++ tests/testthat/test_OptPath.R
@@ -3,7 +3,6 @@ context("OptPath")
 test_that("test if OptPath works", {
   op = th.opt.path.full
 
-  if (FALSE) {
   # test active bindings
   expect_equal(op$x.names, names(th.paramset.flat.full$params))
   expect_equal(op$length, 10)
@@ -15,11 +14,10 @@ test_that("test if OptPath works", {
 
   # contents
   op.df = as.data.frame(op)
+  expect_data_frame(op.df, nrow = 10, ncol = 13)
   expect_set_equal(colnames(op.df), c("dob", "message", "error", "exec.time", "timestamp", "th.param.int", "th.param.real", "th.param.categorical", "th.param.flag", "th.param.int.trafo", "th.param.real.trafo", "th.param.categorical.trafo", "y", 'th.ex1', 'th.ex2'))
   expect_equal(op.df$y, 1:10)
   expect_class(op.df$timestamp, "POSIXct")
-  }
-
   
 })
 
