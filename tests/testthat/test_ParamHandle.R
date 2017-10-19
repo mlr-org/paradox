863799cdbfff4e33c8e7d859abccfeeda7aadad6
smilesun <smilesun.east@gmail.com>
2017-10-19 12:55:17 +0200

    remove id in ParamHandle test

--- tests/testthat/test_ParamHandle.R
+++ tests/testthat/test_ParamHandle.R
@@ -1,7 +1,7 @@
 context("ParamHandle")
 
-test_that("test if ParamHandle constructor works", {
-  # this is a ill example,do not use in client code!
+test_that("ParamHandle constructor, addChild, toStringVal", {
+  # this is an ill example,do not use in client code!
   ps = ParamHandle$new(id = "Model", val = "SVM")
   ps$addMandChild(ParamHandle$new(id = "C", val = 0.3))
   kernel = ps$addMandChild(ParamHandle$new(id = "kernel", val = "rbf"))
@@ -26,12 +26,12 @@ test_that("test if ParamHandle constructor works with SimpleParamNode", {
 
 
 
-test_that("test if ParamHandle toStringVal behaves normal without value", {
+test_that("ParamHandle toStringVal behaves normal without value", {
   ps = ParamHandle$new(id = "Model", node = ParamCategorical$new(id = "Model", values = c("SVM", "RF")), val = "SVM")
-  ps$addCondChild(ParamHandle$new(id = "C", node = ParamReal$new(id = "C", lower = 0, upper = 100), depend = list(id = "Model", val = "SVM")))
-  kernel = ps$addCondChild(ParamHandle$new(id = "kernel", node = ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "Model", val = "SVM")))
-  kernel$addCondChild(ParamHandle$new(id = "gamma", ParamReal$new(id = "gamma", lower = 0, upper = 100), depend = list(id = "kernel", val = "rbf")))
-  kernel$addCondChild(ParamHandle$new(id = "n", node = ParamInt$new(id = "n", lower = 1L, upper = 10L), depend = list(id = "kernel", val = "poly")))
+  ps$addCondChild(ParamHandle$new(node = ParamReal$new(id = "C", lower = 0, upper = 100), depend = list(id = "Model", val = "SVM")))
+  kernel = ps$addCondChild(ParamHandle$new(node = ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "Model", val = "SVM")))
+  kernel$addCondChild(ParamHandle$new(node = ParamReal$new(id = "gamma", lower = 0, upper = 100), depend = list(id = "kernel", val = "rbf")))
+  kernel$addCondChild(ParamHandle$new(node = ParamInt$new(id = "n", lower = 1L, upper = 10L), depend = list(id = "kernel", val = "poly")))
   ps$toStringVal()
 })
 
