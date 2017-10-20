880f36181d4ca74098a34a0bf7d779d1a8819bd1
smilesun <smilesun.east@gmail.com>
2017-10-20 18:45:37 +0200

    delete not used test

--- tests/testthat/test_ParamParseFromFlat.R
+++ tests/testthat/test_ParamParseFromFlat.R
@@ -5,7 +5,7 @@ test_that("test if Param parse from flat works with sample", {
   # self$host$id == arg$depend$id is the only condition to addConchild, if depend is NULL, addMandChild
   input = list(
     list(node = ParamCategorical$new(id = "model", values = c("SVM", "RF"))),
-    list(node = ParamReal$new(id = "C", lower = 0, upper = 100), val = 3, depend = list(id = "model", val = "SVM")),
+    list(node = ParamReal$new(id = "C", lower = 0, upper = 100), depend = list(id = "model", val = "SVM")),
     list(node = ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "model", val = "SVM")),
     list(node = ParamReal$new(id = "gamma", lower = 0, upper = 100), depend = list(id = "kernel", val = "rbf"))
  ) 
