64820d0ec59706b3cf91ba5fba83c4087e71e6e8
smilesun <smilesun.east@gmail.com>
2018-04-19 18:08:47 +0200

    simpler api

--- R/ParamTree_functions.R
+++ R/ParamTree_functions.R
@@ -1,29 +1,35 @@
 #' @title  ParamTree Factory method
-#' 
+#'
 #' @description Define a list of Node in the hyper-parameter definition
-#' 
+#'
 #' @param ... set of ParamTreeDn
 #' @return the root node of the ParamTree
-#' @export 
+#' @export
 ParamTreeFac = function(...) {
   input = list(...)
-  lapply(input, function(x) { assertTRUE(test_class(x, "ParamSimple") | test_class(x, "NodeWithDependency")) })
+  lapply(input, function(x) {
+    assertTRUE(test_class(x, "ParamSimple") | test_class(x, "NodeWithDependency")) })
   ps = ParamHandle$new(id = "Root")  #FIXME: id should be defined outside the code
   ps$visitor$parseFlat(input)
   return(ps$getFirstMandChild)
 }
 
 #' @title make conditional tree node
-#' 
+#'
 #' @description Define a node in the hyper parameter tree with dependencies
-#' 
+#'
 #' @param node ParamSimple
 #' @param depend A list with id and val representing the dependency for the current node
 #' @return List of class NodeParamSetTree
-#' @export 
+#' @export
 makeCondTreeNode = function(node, depend = NULL) {
   node = list(node = node, depend = depend)
   class(node) = "NodeWithDependency"
   return(node)
+}
+
+#' @export
+makeCondTreeNode2 = function(node, did, expr) {
+  makeCondTreeNode(node = node, depend = list(id = did, fun = fun))
 }
 
