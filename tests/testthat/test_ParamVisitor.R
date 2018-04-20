7161bbf4bef2a74af9f867ed1c3a54e5b00ac98f
smilesun <smilesun.east@gmail.com>
2018-04-20 11:48:11 +0200

    fix unit test

--- tests/testthat/test_ParamVisitor.R
+++ tests/testthat/test_ParamVisitor.R
@@ -5,11 +5,11 @@ test_that("test if Param parse from flat works with sample", {
   # self$host$id == arg$depend$id is the only condition to addConchild, if depend is NULL, addMandChild
   input = list(
     list(node = ParamCategorical$new(id = "model", values = c("SVM", "RF"))),
-    list(node = ParamReal$new(id = "C", lower = 0, upper = 100), depend = list(id = "model", fun = quote(model == "SVM")),
+    list(node = ParamReal$new(id = "C", lower = 0, upper = 100), depend = list(id = "model", fun = quote(model == "SVM"))),
     list(node = ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), depend = list(id = "model", fun = quote(model == "SVM"))),
-    list(node = ParamReal$new(id = "gamma", lower = 0, upper = 100), depend = list(id = "kernel", fun = quote(kernel == "rbf")),
-    list(node = ParamInt$new(id = "n", lower = 1L, upper = 10L), depend = list(id = "kernel", fun = quote(kernel == "poly")),
-    list(node = ParamInt$new(id = "ntree", lower = 1L, upper = 10L), depend = list(id = "model", fun = quote(model == "RF"))
+    list(node = ParamReal$new(id = "gamma", lower = 0, upper = 100), depend = list(id = "kernel", fun = quote(kernel == "rbf"))),
+    list(node = ParamInt$new(id = "n", lower = 1L, upper = 10L), depend = list(id = "kernel", fun = quote(kernel == "poly"))),
+    list(node = ParamInt$new(id = "ntree", lower = 1L, upper = 10L), depend = list(id = "model", fun = quote(model == "RF")))
  )
   ps$visitor$parseFlat(input)
   ps$getFirstMandChild$sample()
