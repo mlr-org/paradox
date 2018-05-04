ba3a058793eda3152e9c7ab957c37fc5d9c0156e
smilesun <smilesun.east@gmail.com>
2018-05-04 17:35:23 +0200

    add more unit test

--- tests/testthat/test_ParamTree.R
+++ tests/testthat/test_ParamTree.R
@@ -32,6 +32,35 @@ test_that("test if two ParamTree works", {
   pt$toStringVal()
 })
 
+test_that("ParamSetTree constructor works", {
+   pst = ParamSetTree$new("test",
+       ParamCategorical$new(id = "model", values = c("SVM", "RF")),
+       addDep(ParamReal$new(id = "C", lower = 0, upper = 100), 
+         did = "model", expr = quote(model == "SVM")), # did here means dependant id
+       addDep(ParamCategorical$new(id = "kernel", values = c("rbf", "poly")), 
+         did = "model", expr = quote(model == "SVM")),
+       addDep(ParamReal$new(id = "gamma", lower = 0, upper = 101), 
+         did = "kernel", expr = quote(kernel == "rbf")),
+       addDep(ParamInt$new(id = "n", lower = 1L, upper = 10L), 
+         did = "kernel", expr = quote(kernel == "poly")),
+       addDep(ParamInt$new(id = "n_tree", lower = 1L, upper = 10L), 
+         did = "model", expr = quote(model == "RF"))
+       )
+   pst$sample(1L)
+   pst$getFlatList()
+   pst$toStringVal()
+   pst$sample(10L)
+})
+
+test_that("recursive para works", {
+  ps = recursiveParaFac(nr = 2,
+      ParamInt$new(id = "layer_dense.units", lower = 2L, upper = 1000L),
+      ParamReal$new(id = "kernel_regularizer", lower = 0, upper = 3.0), 
+      ParamReal$new(id = "bias_regularizer", lower = 0, upper = 3.0), 
+      ParamCategorical$new(id = "reg_type", values = c("regularizer_l1", "regularizer_l2")),
+      ParamCategorical$new(id = "activation_fun", values = c("sigmoid", "tanh", "linear")))
+  ps$sample(3L) 
+})
 # test_that("test if two ParamTree works", {
 #   ps = recursiveParaFac(2,
 #       ParamCategorical$new(id = "model", values = c("SVM", "RF")),
