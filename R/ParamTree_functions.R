970afd14102be74092ca22d3b81b5fcd066e6dd4
smilesun <smilesun.east@gmail.com>
2018-05-08 02:13:33 +0200

    export function

--- R/ParamTree_functions.R
+++ R/ParamTree_functions.R
@@ -22,6 +22,7 @@ ParamTreeFac = function(id, ...) {
 #' @param did dependent node id
 #' @param expr expression that provides the dependency
 #' @return List of class NodeParamSetTree
+#' @export
 addDep = function(node, did, expr) {
   makeCondTreeNode(node = node, depend = list(id = did, fun = expr))
 }
