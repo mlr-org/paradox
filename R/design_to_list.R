c7c39fdd7d6efb588b6096e94071420d95618055
Jakob Richter <code@jakob-r.de>
2018-10-12 16:15:21 +0200

    complete combine and subset

--- R/design_to_list.R
+++ R/design_to_list.R
@@ -1,3 +1,11 @@
+#' @title Design to List
+#' @description
+#' Converts a design or any `data.table` to a list where each list element contains one row.
+#'
+#' @param design [`data.table`]:
+#'   The `data.table` that should be converted
+#'
+#' @return `list`
 #' @export
 design_to_list = function(design) {
   assert_data_table(design)
--- tests/testthat/test_ParamSet.R
+++ tests/testthat/test_ParamSet.R
@@ -10,7 +10,7 @@ test_that("methods and active bindings work", {
     th_paramset_numeric,
     th_paramset_trafo,
     th_paramset_trafo_dictionary
-    )
+  )
   for (ps in ps_list) {
     if (ps$id == "th_paramset_full") {
       expect_equal(ps$ids, c('th_param_int', 'th_param_real', 'th_param_categorical', 'th_param_flag'))
@@ -100,4 +100,118 @@ test_that("repeated params in ParamSet works", {
   expect_list(xs_t$vector_param)
   xs_l = design_to_list(xs_t)
   expect_list(xs_l, len = 10)
+})
+
+test_that("param subset in ParamSet works", {
+  # Define all the different subsets we want to try:
+  configs = list(
+    list(
+      ps = th_paramset_full,
+      ids = c("th_param_int", "th_param_flag"),
+      expected_ids = c("th_param_int", "th_param_flag"),
+      fix = NULL
+    ),
+    list(
+      ps = th_paramset_full,
+      expected_ids = c("th_param_real", "th_param_categorical", "th_param_flag"),
+      fix = list("th_param_int" = 1L)
+    ),
+    list(
+      ps = th_paramset_trafo,
+      ids = c("th_param_int"),
+      expected_ids = c("th_param_int"),
+      fix = list("th_param_real" = 1)
+    ),
+    list(
+      ps = th_paramset_trafo,
+      ids = NULL,
+      expected_ids = c("th_param_int"),
+      fix = list("th_param_real" = 1)
+    ),
+    list(
+      ps = th_paramset_trafo_dictionary,
+      ids = NULL,
+      expected_ids = c("th_param_int"),
+      fix = list("th_param_real" = 1)
+    ),
+    list(
+      ps = th_paramset_restricted,
+      ids = NULL,
+      expected_ids = c("th_param_int", "th_param_categorical"),
+      fix = list("th_param_real" = 1)
+    )
+  )
+  # Test the different combinations:
+  for (conf in configs) {
+    paramset_sub = conf$ps$subset(ids = conf$ids, fix = conf$fix)
+    expect_equal(paramset_sub$ids, conf$expected_ids)
+    x = paramset_sub$sample(2)
+    expect_set_equal(colnames(x), conf$expected_ids)
+    expect_true(paramset_sub$check(x[1,]))
+    expect_true(paramset_sub$check(x[2,]))
+    x_trafo = paramset_sub$transform(x)
+
+
+    x = paramset_sub$sample(1)
+    expect_set_equal(colnames(x), conf$expected_ids)
+    expect_true(paramset_sub$check(x))
+    x_trafo = paramset_sub$transform(x)
+  }
+})
+
+test_that("Combine of ParamSet work", {
+  # define some ParamSets we will join to the th_ ones
+  new_param_sets = list(
+    normal = ParamSet$new(
+      id = "new_param_set",
+      params = list(
+        ParamReal$new("new_int", lower = 0L, upper = 10L)
+      )
+    ),
+    trafo = ParamSet$new(
+      id = "new_param_set_trafo",
+      params = list(
+        ParamReal$new("new_real", lower = 0, upper = 10)
+      ),
+      trafo = function(x, dict, tags) {
+        x$new_real = sqrt(x$new_real)
+        return(x)
+      }
+    ),
+    restriction = ParamSet$new(
+      id = "new_param_set_requires",
+      params = list(
+        ParamReal$new("new_real", lower = 0, upper = 10),
+        ParamReal$new("new_int", lower = 0L, upper = 10L)
+      ),
+      restriction = quote(new_real>=new_int)
+    )
+  )
+
+  ps_list = list(
+    th_paramset_empty,
+    th_paramset_full,
+    th_paramset_repeated,
+    th_paramset_restricted,
+    th_paramset_numeric,
+    th_paramset_trafo,
+    th_paramset_trafo_dictionary
+  )
+
+  for (ps in ps_list) {
+    for(ps_new in new_param_sets) {
+      ps_comb1 = ps$combine(ps_new)
+      ps_comb2 = ps_new$combine(ps)
+      expect_set_equal(ps_comb1$ids, ps_comb1$ids)
+      expect_set_equal(ps_comb1$ids, c(ps$ids, ps_new$ids))
+      x = ps_comb1$sample(1)
+      expect_data_table(x)
+      expect_true(ps_comb1$check(x))
+      expect_true(ps_comb2$check(x))
+      xt1 = ps_comb1$transform(x)
+      xt2 = ps_comb2$transform(x)[, colnames(xt1), with = FALSE]
+      expect_equal(xt1, xt2)
+    }
+  }
+
 })
