0f803dca69a859fbf8bbc902579196499641868f
Jakob Richter <code@jakob-r.de>
2018-12-01 12:40:06 +0100

    doc for design_to_list

--- R/design_to_list.R
+++ R/design_to_list.R
@@ -1,11 +1,11 @@
-#' @title Design to List
+#' @title Convert Design to List
+#'
 #' @description
-#' Converts a design or any `data.table` to a list where each list element contains one row.
+#' Convert design to a list where each list item represents one line of the design.
 #'
-#' @param design [`data.table`]:
-#'   The `data.table` that should be converted
-#'
-#' @return `list`
+#' @param design [\code{data.table}]\cr
+#'   The object that contains the design.
+#' @return list
 #' @export
 design_to_list = function(design) {
   assert_data_table(design)
