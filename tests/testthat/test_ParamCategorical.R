e9c135ecef5de86cb69fd3b9df69d0146096d386
Bernd Bischl <bernd_bischl@gmx.net>
2018-12-03 01:26:02 +0100

    ParamCateg: add more tests

--- tests/testthat/test_ParamCategorical.R
+++ tests/testthat/test_ParamCategorical.R
@@ -3,4 +3,19 @@ context("ParamCateg")
 test_that("test if ParamCateg constructor works", {
   p = ParamCateg$new(id = "test", values = c("a", "b"))
   expect_equal(p$values, c("a", "b"))
+  expect_equal(p$nlevels, 2L)
+  expect_true(p$has_finite_bounds)
+
+  # we dont allow NAs as values
+  expect_error(ParamCateg$new(id = "test", values = c("a", NA)))
 })
+
+test_that("printer works", {
+  p = ParamCateg$new(id = "x", values = c("a", "b"))
+  expect_output(print(p), "x \\[character\\]")
+  expect_output(print(p), "\\{a, b\\}")
+})
+
+
+
+
