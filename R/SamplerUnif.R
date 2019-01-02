bfb3d8f17ff3c531b89bd682f443fa1bf5d50551
Bernd Bischl <bernd_bischl@gmx.net>
2019-01-02 04:16:26 +0100

    hierachical sampler

--- R/SamplerUnif.R
+++ R/SamplerUnif.R
@@ -1,9 +1,10 @@
 #' @title Sampler: Multivariate uniform.
-#' @format [R6Class] object. Inherits from [SamplerJointIndep].
+#' @format [R6Class] object. Inherits from [SamplerHierachical].
 #'
 #' @description
 #' Uniform random sampling for arbitrary (bounded) param sets.
-#' Constructs 1 uniform sampler per param, then passes them to [SamplerJointIndep].
+#' Constructs 1 uniform sampler per param, then passes them to [SamplerHierachical].
+#' Hence, also works for param sets with dependencies.
 #'
 #' @section Public methods:
 #' * `new(param_set)` \cr
@@ -12,12 +13,12 @@
 #' @name SamplerUnif
 #' @family Sampler
 #' @export
-SamplerUnif = R6Class("SamplerUnif", inherit = SamplerJointIndep,
+SamplerUnif = R6Class("SamplerUnif", inherit = SamplerHierachical,
   public = list(
     initialize = function(param_set) {
       assert_paramset(param_set, must_bounded = TRUE, no_deps = TRUE, no_untyped = TRUE)
       samplers = lapply(param_set$params, Sampler1DUnif$new)
-      super$initialize(samplers)
+      super$initialize(param_set, samplers)
     }
   )
 )
