f8cd05bb3fdfb085d028e33bf4e9c8a1beb477d8
mb706 <mlr.developer@mb706.com>
2019-01-09 10:30:19 +0100

    playing around with nonuniform samplers

--- attic/sampler.R
+++ attic/sampler.R
@@ -115,3 +115,34 @@ plot(s$test_dsc, s$test_cnt)
 s$test_dsc = as.numeric(s$test_dsc) + runif(nrow(s)) / 2
 plot(s$test_dsc, s$test_cnt)
 
+# ------
+
+
+ps = ParamSet$new(
+  list(
+    ParamDbl$new("test_cnt", lower = 0, upper = 1, special_vals = list("a"), default = "a", tags = letters[1:2]),
+    ParamInt$new("test_int", lower = 0, upper = 10, special_vals = list("b"), default = 2, tags = letters[1:4]),
+    ParamFct$new("test_dsc", values = letters[1:3], default = "d", special_vals = list("d"),  tags = letters[2:3]),
+    ParamLgl$new("test_lgl", special_vals = list("c"), default = FALSE, tags = letters[3:4])
+  )
+)
+
+plot(Sampler1DUnif$new(ps$params$test_cnt)$sample(10))
+Sampler1DUnif$new(ps$params$test_int)
+Sampler1DUnif$new(ps$params$test_dsc)
+Sampler1DUnif$new(ps$params$test_lgl)
+
+Sampler1DCateg$new(ps$params$test_dsc)
+plot(Sampler1DCateg$new(ps$params$test_lgl, prob = c(.1, .9))$sample(1000))
+
+Sampler1DTruncNorm$new(ps$params$test_int)  # XXX
+
+
+Sampler1DTruncNorm$new(ps$params$test_dbl)
+
+
+
+
+
+
+
