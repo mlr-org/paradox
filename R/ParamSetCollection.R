3a9dd389350913eb0b4770811955d051e169f93a
Bernd Bischl <bernd_bischl@gmx.net>
2019-01-10 02:20:15 +0100

    ...

--- R/ParamSetCollection.R
+++ R/ParamSetCollection.R
@@ -1,42 +1,46 @@
-# allows user to set up a collection of sets.
-# FIXME: can we somehow simply "delegate" to the sets below?
-# without explicitly storing a combined set? mayve we can write an AB "params" and "deps"??
-# FIXME: we should probably not allow the user to add individual params, only sets?
-# FIXME: should the set allow the setting of params? i guess in tuningwe need this?
-# FIXME: i guess we dont allow trafos, we dont allow to set them? but must execute all of them?
-# FIXME: disallow:
-# - add_dep
-#  - add(param), maybe allow add(set)
-# - trafo = foo
+# FIXME: we need to be able to set a trafo in the complete collection, but maybe then no
+# trafos should exist on the individuell sets?
 
+# FIXME: check get deps, and on deps
 
+# FIXME: can we cache the params in the collection? so we dont have to build them all on the fly?
 
-# ParamSetCollection = R6Class("ParamSetCollection", inherit = ParamSet,
-#   public = list(
-#     initialize = function(sets) {
-#       assert_list(sets, types = "ParamSet")
-#       setids = map_chr(sets, "set_id")
-#       assert_names(setids, type = "unique")
-#       private$.sets = sets
-#       combined = ParamSet$new()
-#       for (i in 1:length(sets)) {
-#         s = sets[[i]]
-#         for (j in seq_along(s$params)) {
-#           p = s$params[[j]]
-#           pid = paste(s$set_id, p$id, sep = ".") #FIXME: which sep? use ":" ?
-#           p$id = pid; names(s$params)[j] = pid
-#         }
-#         print(s)
-#         combined$add(s)
-#       }
-#       self$params = combined$params
-#       self$deps = combined$deps
-#     }
-#     add_dep()
+# FIXME: we need to be able to add deps across the sets, i fear. especially for the brnaching
 
-#   ),
-#   private = list(
-#     .sets = NULL
-#   )
+# FIXME: if we add deps, what format / names do we have to use to stay valied?
 
-# )
+ParamSetCollection = R6Class("ParamSetCollection", inherit = ParamSet,
+  public = list(
+    initialize = function(sets) {
+      assert_list(sets, types = "ParamSet")
+      setids = map_chr(sets, "set_id")
+      assert_names(setids, type = "unique")
+      private$.sets = sets
+      private$build_params()
+      private$build_deps()
+    },
+
+    add = function(p) stop("not allowed"),
+
+    add_dep = function(p) stop("not allowed")
+
+  ),
+
+  private = list(
+    .sets = NULL,
+    build_params = function() {
+      private$.params = unlist(map(private$.sets, function(s)  {
+        ps = map(s$params, function(p) p$clone(deep = TRUE))
+        for (p in ps)
+          p$id = paste(s$set_id, p$id, sep = ".")
+        set_names(ps, map_chr(ps, "id"))
+      }), recursive = FALSE)
+    },
+
+    build_deps = function() {
+      private$.deps = unlist(map(private$.sets, function(s)  {
+        ps = map(s$deps, function(p) p$clone(deep = TRUE))
+      }), recursive = FALSE)
+    }
+  )
+)
