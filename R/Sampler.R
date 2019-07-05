8ae9cd7a112318bb14efc9e7958d8cb878470f83
pat-s <patrick.schratz@gmail.com>
2019-07-05 11:09:39 +0200

    Sample.R: upd doc

--- R/Sampler.R
+++ R/Sampler.R
@@ -6,23 +6,20 @@
 #' @description
 #' Random sampler for an arbitrary [ParamSet].
 #'
-#' @section Public members / active bindings:
+#' @section Construction:
+#'
+#' ```
+#' s = Sampler$new(param_set)
+#' ```
+#'
 #' * `param_set`            :: [ParamSet] \cr
 #'   Domain / support of the distribution we want to sample from.
 #'
-#' @section Public methods:
-#' * `new(param_set)` \cr
-#'   [ParamSet] -> `self` \cr
-#'   Abstract, only inheriting subclasses call this.
-#'   Param set is cloned on construction.
+#' @section Methods:
+#'
 #' * `sample(n)` \cr
 #'   `integer(1)` -> [Design] \cr
 #'   Sample n values from the distribution.
-#'
-#' @section Private methods / Internals:
-#' * `.sample(n)` \cr
-#'   `integer(1)` -> [data.table] \cr
-#'   Inheriting sublcasses have to implement thus, called from `sample()`
 #'
 #' @family Sampler
 #' @export
