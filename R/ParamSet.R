1eb94142f116feebc04c946b6cb036c3d2942aee
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-08-15 21:56:49 +0100

    Params -> Sets

--- R/ParamSet.R
+++ R/ParamSet.R
@@ -9,7 +9,7 @@
 #'
 #' @section Construction:
 #' ```
-#' ParamSet$new(params = named_list())
+#' ParamSet$set(params = named_list())
 #' ```
 #'
 #' * `params` :: named `list()`\cr
@@ -119,10 +119,10 @@
 #'     - storage_type: character
 #'     - tags: list col of character vectors
 #' @examples
-#' ps = ParamSet$new(
+#' ps = ParamSet$set(
 #'   params = list(
-#'     ParamDbl$new("d", lower = -5, upper = 5, default = 0),
-#'     ParamFct$new("f", levels = letters[1:3])
+#'     ParamDbl$set("d", lower = -5, upper = 5, default = 0),
+#'     ParamFct$set("f", levels = letters[1:3])
 #'   )
 #' )
 #'
@@ -131,25 +131,27 @@
 #'   return(x)
 #' }
 #'
-#' ps$add(ParamInt$new("i", lower = 0L, upper = 16L))
+#' ps$add(ParamInt$set("i", lower = 0L, upper = 16L))
 #'
 #' ps$check(list(d = 2.1, f = "a", i = 3L))
 #' @export
-ParamSet = R6Class("ParamSet",
-  public = list(
-    initialize = function(params = named_list()) {
-      assert_list(params, types = "Param")
-      ids = map_chr(params, "id")
-      assert_names(ids, type = "strict")
-      private$.params = set_names(map(params, function(p) p$clone(deep = TRUE)), ids)
+ParamSet <- R6Class("ParamSet")
+ParamSet$set("public","initialize",function(...){
+      params = as.list(sys.call(1))
+      params[[1]] = NULL
+      if(length(params)==0)
+        stop("ParamSet can't be empty.")
+      params = map(params, eval)
+      invisible(map(params, assertSetInterval))
+      assert_names(names(params), type = "strict")
+      private$.params = params
       self$set_id = ""
-    },
-
-    add = function(p) {
+    }) # DONE
+ParamSet$set("public","add",function(p) {
 
       assert_multi_class(p, c("Param", "ParamSet"))
       p = if (inherits(p, "Param")) { # level-up param to set
-        ParamSet$new(list(p))
+        ParamSet$set(list(p))
       } else {
         p$clone(deep = TRUE)
       }
@@ -163,9 +165,8 @@ ParamSet = R6Class("ParamSet",
       private$.values = c(private$.values, p$values)
       private$.deps = rbind(private$.deps, p$deps)
       invisible(self)
-    },
-
-    ids = function(class = NULL, is_bounded = NULL, tags = NULL) {
+    }) # TO DO
+ParamSet$set("public","ids",function(class = NULL, is_bounded = NULL, tags = NULL) {
 
       ids = names(self$params)
       if (is.null(class) && is.null(is_bounded) && is.null(tags)) {
@@ -181,7 +182,7 @@ ParamSet = R6Class("ParamSet",
 
       if (!is.null(is_bounded)) {
         assert_flag(is_bounded)
-        ii = ii & map_lgl(self$params, "is_bounded")
+        ii = ii & self$is_bounded
       }
 
       if (!is.null(tags)) {
@@ -190,14 +191,12 @@ ParamSet = R6Class("ParamSet",
       }
 
       ids[ii]
-    },
-
-    get_values = function(class = NULL, is_bounded = NULL, tags = NULL) {
+    }) # TO DO
+ParamSet$set("public","get_values",function(class = NULL, is_bounded = NULL, tags = NULL) {
       values = self$values
       values[intersect(names(values), self$ids(class = class, is_bounded = is_bounded, tags = tags))]
-    },
-
-    subset = function(ids) {
+    }) # TO DO
+ParamSet$set("public","subset",function(ids) {
       param_ids = names(self$params)
       assert_subset(ids, param_ids)
       if (self$has_deps) { # check that all required / leftover parents are still in new ids
@@ -211,9 +210,8 @@ ParamSet = R6Class("ParamSet",
       ids2 = union(intersect(ids, names(private$.values)), setdiff(names(private$.values), param_ids)) # restrict to ids already in pvals
       private$.values = private$.values[ids2]
       invisible(self)
-    },
-
-    check = function(xs) {
+    }) # TO DO
+ParamSet$set("public","check",function(xs) {
 
       ok = check_list(xs, names = "unique")
       if (!isTRUE(ok)) {
@@ -238,9 +236,10 @@ ParamSet = R6Class("ParamSet",
 
       # check each parameters feasibility
       for (n in ns) {
-        ch = self$params[[n]]$check(xs[[n]])
-        if (test_string(ch)) { # we failed a check, return string
-          return(paste0(n, ": ", ch))
+        ch = self$params[[n]]$liesInSetInterval(xs[[n]])
+        if (!ch) { # we failed a check, return string
+          return(sprintf("The value %s is not contained in the Set/Interval %s", xs[[n]],
+                         self$params[[n]]$strprint()))
         }
       }
 
@@ -254,25 +253,28 @@ ParamSet = R6Class("ParamSet",
           # - if param is there, then parent must be there, then cond must be true
           # - if param is not there
           cond = deps$cond[[j]]
-          ok = (p1id %in% ns && p2id %in% ns && cond$test(xs[[p2id]])) ||
+          ok = (p1id %in% ns && p2id %in% ns && xs[[p2id]] == cond) ||
             (p1id %nin% ns)
           if (isFALSE(ok)) {
             val = xs[[p2id]]
             val = ifelse(is.null(val), "<not-there>", val)
-            return(sprintf("Condition for '%s' not ok: %s %s %s; instead: %s=%s",
-              p1id, p2id, cond$type, str_collapse(cond$rhs), p2id, val))
+            return(sprintf("Condition for '%s' not ok: %s -> %s = %s; instead: %s=%s",
+              p1id, p1id, p2id, cond, p2id, val))
           }
         }
       }
 
       return(TRUE) # we passed all checks
-    },
-
-    test = function(xs) makeTest(res = self$check(xs)),
-
-    assert = function(xs, .var.name = vname(xs)) makeAssertion(xs, self$check(xs), .var.name, NULL),
-
-    add_dep = function(id, on, cond) {
+    }) # UNCHANGED
+ParamSet$set("public","test",function(xs){
+  makeTest(res = self$check(xs))
+}) # UNCHANGED
+ParamSet$set("public","assert",function(xs, .var.name = vname(xs)){
+  makeAssertion(xs, self$check(xs), .var.name, NULL)
+}) # UNCHANGED
+ParamSet$set("public","add_dep",function(id, on, cond) {
+      id <- as.character(substitute(id))
+      on <- as.character(substitute(on))
       ids = names(self$params)
       assert_choice(id, ids)
       assert_choice(on, ids)
@@ -281,45 +283,38 @@ ParamSet = R6Class("ParamSet",
       }
       private$.deps = rbind(private$.deps, data.table(id = id, on = on, cond = list(cond)))
       invisible(self)
-    },
-
-    # printer, prints the set as a datatable, with the option to hide some cols
-    print = function(..., hide_cols = c("nlevels", "is_bounded", "special_vals", "tags", "storage_type")) {
+    }) # DONE - Now uses substitute
+ParamSet$set("public","print",function(..., hide_cols = c("special_vals","tags","parents")) {
       catf("ParamSet: %s", self$set_id)
       if (self$is_empty) {
         catf("Empty.")
       } else {
         d = as.data.table(self)
-        assert_subset(hide_cols, names(d))
         if (self$has_deps) { # add a nice extra charvec-col to the tab, which lists all parents-ids
           dd = self$deps[, .(parents = list(unlist(on))), by = id]
-          d = merge(d, dd, on = "id", all.x = TRUE)
+          d = merge(d, dd, on = "id", all.x = TRUE, sort = FALSE)
         }
-        v = named_list(d$id) # add values to last col of print-dt as list col
-        v = insert_named(v, self$values)
-        d$value = list(v)
+        # v = named_list(d$id) # add values to last col of print-dt as list col
+        # v = insert_named(v, self$values)
+        # d$value = list(v)
+        d$support <- map(d$support, strprint)
+        d[d == "NULL"] = "-"
+
+        assert_subset(hide_cols, names(d))
         print(d[, setdiff(colnames(d), hide_cols), with = FALSE])
       }
       if (!is.null(self$trafo)) {
         catf("Trafo is set.")
-      } # printing the trafa functions sucks (can be very long). dont see a nother option then to suppress it for now
-    }
-  ),
+      } # printing the trafo functions sucks (can be very long). dont see a nother option then to suppress it for now
+    }) # DONE
 
-  active = list(
-    params = function() {
+ParamSet$set("active","params",function() {
       private$.params
-    },
-
-    deps = function(v) {
-      if (missing(v)) {
-        private$.deps
-      } else {
-        private$.deps = assert_data_table(v)
-      }
-    },
-
-    set_id = function(v) {
+    }) # DONE - UNCHANGED
+ParamSet$set("active","deps",function() {
+   return(private$.deps)
+    }) # DONE - Now getter only, setter above
+ParamSet$set("active","set_id",function(v) {
       if (missing(v)) {
         private$.set_id
       } else {
@@ -329,78 +324,73 @@ ParamSet = R6Class("ParamSet",
         }
         private$.set_id = v
       }
-    },
-
-    length = function() {
+    }) # Is this the id of the ParamSet? Or is it a setter for Param IDs?
+ParamSet$set("active","length", function() {
       length(self$params)
-    },
-
-    is_empty = function() {
+    }) # What's the point of this?
+ParamSet$set("active","is_empty",function() {
       length(self$params) == 0L
-    },
+    }) # Deprecated? ParamSet can't be constructed empty
+ParamSet$set("active","class",function() {
+  map(self$params,getR6Class)
+}) # DONE - Now returns R6Class name
+ParamSet$set("active","lower",function() {
+  lapply(self$params, function(y) y$inf())
+}) # DONE
+ParamSet$set("active","upper",function() {
+  lapply(self$params, function(y) y$sup())
+}) # DONE
+ParamSet$set("active","levels",function() {
+  lapply(self$params, function(y) y$elements())
+    }) # DONE
+ParamSet$set("active","nlevels",function() {
+  lapply(self$params, function(y) length(y$elements()))
+    }) # DONE
+ParamSet$set("active","is_bounded", function() {
+  map(self$params, testBounded)
+}) # DONE
+ParamSet$set("active", "special_vals", function(xs) {
+  if (missing(xs)) {
+    return(private$.special_vals)
+  }
 
-    class = function() {
-      private$get_member_with_idnames("class", as.character)
-    },
-
-    lower = function() {
-      private$get_member_with_idnames("lower", as.double)
-    },
-
-    upper = function() {
-      private$get_member_with_idnames("upper", as.double)
-    },
-
-    levels = function() {
-      private$get_member_with_idnames("levels", as.list)
-    },
-
-    nlevels = function() {
-      private$get_member_with_idnames("nlevels", as.double)
-    },
-
-    is_bounded = function() {
-      all(map_lgl(self$params, "is_bounded"))
-    },
-
-    special_vals = function() {
-      private$get_member_with_idnames("special_vals", as.list)
-    },
-
-    default = function() {
+  self$assert(xs)
+  if (length(xs) == 0L) xs = named_list()
+  private$.special_vals = xs
+    }) # DONE - Acts like tag
+ParamSet$set("active","default",function() {
       discard(private$get_member_with_idnames("default", as.list), is_nodefault)
-    },
+    }) # Suggestion: Remove default and instead initialize value when possible
+ParamSet$set("active","tags",function(xs) {
+  if (missing(xs)) {
+    return(private$.tags)
+  }
 
-    tags = function() {
-      private$get_member_with_idnames("tags", as.list)
-    },
-
-    storage_type = function() {
+  self$assert(xs)
+  if (length(xs) == 0L) xs = named_list()
+  private$.tags = xs
+    }) # Can tags be anything? Are they a vector? A specific set of values?
+ParamSet$set("active","storage_type",function() {
       private$get_member_with_idnames("storage_type", as.character)
-    },
-
-    is_number = function() {
-      private$get_member_with_idnames("is_number", as.logical)
-    },
-
-    is_categ = function() {
-      private$get_member_with_idnames("is_categ", as.logical)
-    },
-
-    trafo = function(f) {
+    }) # What's this for?
+ParamSet$set("active","is_number",function() {
+  lapply(self$params, function(x) return(x$class() %in% c("numeric", "integer")))
+}) # DONE
+ParamSet$set("active","is_categ",function() {
+  lapply(self$params, function(x) return(!(x$class() %in% c("numeric", "integer"))))
+}) # DONE
+ParamSet$set("active","trafo",function(f) {
       if (missing(f)) {
         private$.trafo
       } else {
         assert_function(f, args = c("x", "param_set"), null.ok = TRUE)
         private$.trafo = f
       }
-    },
-
-    has_trafo = function() {
+    }) # TO DO
+ParamSet$set("active","has_trafo",function() {
       !is.null(private$.trafo)
-    },
-
-    values = function(xs) {
+    }) # DONE - UNCHANGED
+ParamSet$set("active","values",function(xs) {
       if (missing(xs)) {
         return(private$.values)
       }
@@ -408,37 +398,41 @@ ParamSet = R6Class("ParamSet",
       self$assert(xs)
       if (length(xs) == 0L) xs = named_list()
       private$.values = xs
-    },
-
-    has_deps = function() {
+    }) # DONE - UNCHANGED
+ParamSet$set("active","has_deps",function() {
       nrow(private$.deps) > 0L
-    },
-
-    extra_values = function() {
+    }) # DONE - UNCHANGED
+ParamSet$set("active","extra_values",function() {
       private$.values[names(private$.values) %nin% names(private$.params)]
-    }
-  ),
+    }) # What's the point of this?
 
-  private = list(
-    .set_id = NULL,
-    .trafo = NULL,
-    .params = NULL,
-    .values = named_list(),
-    .deps = data.table(id = character(0L), on = character(0L), cond = list()),
-    # return a slot / AB, as a named vec, named with id (and can enforce a certain vec-type)
-    get_member_with_idnames = function(member, astype) set_names(astype(map(self$params, member)), names(self$params)),
-
-    deep_clone = function(name, value) {
+ParamSet$set("private",".set_id",NULL) # UNCHANGED
+ParamSet$set("private",".trafo", NULL) # UNCHANGED
+ParamSet$set("private",".special_vals", named_list()) # DONE - Added special_vals to ParamSet
+ParamSet$set("private",".tags", named_list()) # DONE - Added tags to ParamSet
+ParamSet$set("private",".params", NULL) # UNCHANGED
+ParamSet$set("private",".values", named_list()) # UNCHANGED
+ParamSet$set("private",".deps",data.table(id = character(0L), on = character(0L), cond = list())) # UNCHANGED
+# return a slot / AB, as a named vec, named with id (and can enforce a certain vec-type)
+ParamSet$set("private","get_member_with_idnames", function(member, astype){
+  set_names(astype(map(self$params, member)), names(self$params))
+}) # UNCHANGED
+ParamSet$set("private","deep_clone", function(name, value) {
       switch(name,
         ".params" = map(value, function(x) x$clone(deep = TRUE)),
         ".deps" = copy(value),
         value
       )
-    }
-  )
-)
+    }) # UNCHANGED
 
 #' @export
 as.data.table.ParamSet = function(x, ...) {
-  map_dtr(x$params, as.data.table)
-}
+  supp <- map(x$params, strprint)
+  val = spe.vals = tags = dep.lst = named_list(x$ids())
+
+  val <- insert_named(val, x$values)
+  spe.vals <- insert_named(spe.vals, x$special_vals)
+  tags <- insert_named(tags, x$tags)
+
+  return(data.table(id = x$ids(), value = val, support = x$params, special_vals = spe.vals, tags = tags))
+} # DONE
