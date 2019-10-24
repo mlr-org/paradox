a9fc144e2387f108cc8b93626396d5abcf589020
Jakob Richter <code@jakob-r.de>
2019-10-24 11:32:48 +0200

    Check rhs of condition in dependency (Issue #252) (#253)
    * Check rhs of condition in dependency #252
    
    * test error message
    
    * improve error message
    
    * improve error message

--- tests/testthat/test_deps.R
+++ tests/testthat/test_deps.R
@@ -112,3 +112,9 @@ test_that("we can also dep on integer", {
   expect_true(ps$check(list(i = 2, d = 5)))
   expect_string(ps$check(list(i = 5, d = 5)))
 })
+
+test_that("deps make sense", {
+  ps = th_paramset_full()
+  expect_error(ps$add_dep("th_param_lgl", "th_param_fct", CondEqual$new("d")), "Condition has infeasible values for th_param_fct")
+  expect_error(ps$add_dep("th_param_lgl", "th_param_int", CondAnyOf$new(5:15)), "Condition has infeasible values for th_param_int")
+})
