89e5168409fc16befdfcbcfd29cb1042938f2ea8
mb706 <mlr.developer@mb706.com>
2020-10-18 20:41:14 +0200

    documenting and some rearrangement

--- R/ps.R
+++ R/ps.R
@@ -1,9 +1,46 @@
 #' @include helper.R
+
+#' @title Construct a ParamSet using Short Forms
+#'
+#' The `ps()` short form constructor uses [`Domain`] objects to construct [`ParamSet`]s in a succinct and readable way.
+#' @param ... ([`Domain`] | [`Param`])\cr
+#'   Named arguments of [`Domain`] or [`Param`] objects. The [`ParamSet`] will be constructed of the given [`Param`]s,
+#'   or of [`Params`]s constructed from the given domains. The names of the arguments will be used as `$id`
+#'   (the `$id` of [`Param`] arguments are ignored).
+#' @param .extra.trafo (`function(x, param_set)`)\cr
+#'   Transformation to set the resulting [`ParamSet`]'s `$trafo` value to. This is in addition to any `trafo` of
+#'   [`Domain`] objects given in `...`, and will be run *after* transformations of individual parameters were performed.
+#' @return A [`ParamSet`] object.
+#' @examples
+#' pars = ps(
+#'   a = p_int(0, 10),
+#'   b = p_int(upper = 20),
+#'   c = p_dbl(),
+#'   e = p_fct(letters[1:3]),
+#'   f = p_uty(custom_check = checkmate::check_function)
+#' )
+#' print(pars)
+#'
+#' pars = ps(
+#'   a = p_dbl(0, 1, trafo = exp),
+#'   b = p_dbl(0, 1, trafo = exp),
+#'   .extra_trafo = function(x, ps) {
+#'     x$c <- x$a + x$b
+#'     x
+#'   }
+#' )
+#'
+#' # See how the addition happens after exp()ing:
+#' pars$trafo(list(a = 0, b = 0))
+#'
+#' @family ParamSet construction helpers
 #' @export
 ps = function(..., .extra_trafo = NULL) {
   args = list(...)
   assert_list(args, names = "unique", types = c("Param", "Domain"))
   assert_function(.extra_trafo, null.ok = TRUE)
+
+  # generate Params (with correct id) from Domain objects
   params = imap(args, function(p, name) {
     if (inherits(p, "Param")) {
       p = p$clone(deep = TRUE)
@@ -16,6 +53,7 @@ ps = function(..., .extra_trafo = NULL) {
 
   paramset = ParamSet$new(params)
 
+  # add Dependencies
   imap(args, function(p, name) {
     if (inherits(p, "Param") || is.null(p$requirements)) return(NULL)
     map(p$requirements, function(req) {
@@ -26,12 +64,15 @@ ps = function(..., .extra_trafo = NULL) {
     })
   })
 
+  # add trafos
   trafos = map(discard(args, function(x) inherits(x, "Param") || is.null(x$trafo)),
     function(p) {
       assert_function(p$trafo)
     })
-
   if (length(trafos) || !is.null(.extra_trafo)) {
+    # the $trafo function iterates through the trafos and applies them
+    # We put the $trafo in a crate() (helper.R) to avoid having a function
+    # with lots of things in its environment.
     paramset$trafo = crate(function(x, param_set) {
       for (trafoing in names(trafos)) {
         if (!is.null(x[[trafoing]])) {
@@ -45,65 +86,166 @@ ps = function(..., .extra_trafo = NULL) {
   paramset
 }
 
+#' @title Domain: Parameter Range without an Id
+#'
+#' @description
+#' A `Domain` object is a representation of a single dimension of a [`ParamSet`]. `Domain` objects are used to construct
+#' [`ParamSet`]s, either through the [`ps()`] short form, or through the [`ParamSet`]`$tune_ps` mechanism (see
+#' [`to_tune()`]). `Domain` corresponds to a [`Param`] object, except it does not have an `$id`, and it *does* have a
+#' `trafo` and dependencies (`requires`) associated with it. For each of the basic [`Param`] classes ([`ParamInt`],
+#' [`ParamDbl`], [`ParamLgl`], [`ParamFct`], and [`ParamUty`]) there is a function constructing a `Domain` object
+#' (`p_int()`, `p_dbl()`, `p_lgl()`, `p_fct()`, `p_uty()`). They each have the same arguments as the corresponding
+#' [`Param`] `$new()` function, except without the `id` argument, and with the the additional parameters `trafo`, and
+#' `requires`.
+#'
+#' The `p_fct` function admits a `levels` argument that goes beyond the `levels` accepted by [`ParamFct`]`$new()`.
+#' Instead of a `character` vector, any atomic vector or list (optionally named) may be given. (If the value is a list
+#' that is not named, the names are inferred using `as.character()` on the values.) The resulting `Domain` will
+#' correspond to a range of values given by the names of the `levels` argument with a `trafo` that maps the `character`
+#' names to the arbitrary values of the `levels` argument.
+#'
+#' `Domain` objects are representations of parameter ranges and are intermediate objects to be used in short form
+#' constructions in [`to_tune()`] and [`ps()`]. Because of their nature, they should not be modified by the user.
+#' The `Domain` object's internals are subject to change and should not be relid upon.
+#'
+#' @param trafo (`function`)\cr
+#'   Single argument function performing the transformation of a parameter. When the `Domain` is used to construct a
+#'   [`ParamSet`], this transformation will be applied to the corresponding parameter as part of the `$trafo` function.
+#' @param requires (`call` | `expression`)\cr
+#'   An expression indicating a requirement for the parameter that will be constructed from this. Can be given as an
+#'   expression (using `quote()`), or the expression can be entered directly and will be parsed using NSE (see
+#'   examples). The expression may be of the form `<Param> == <value>` or `<Param> %in% <values>`, which will result in
+#'   dependencies according to `ParamSet$add_dep(on = "<Param>", cond = CondEqual$new(<value>))` or
+#'   `ParamSet$add_dep(on = "<Param>", cond = CondAnyOf$new(<values>))`, respectively (see [`CondEqual`],
+#'   [`CondAnyOf`]). The expression may also contain multiple conditions separated by `&&`.
+#' @examples
+#' params = ps(
+#'   unbounded_integer = p_int(),
+#'   bounded_double = p_dbl(0, 10),
+#'   half_bounded_integer = p_dbl(, 1),
+#'   half_bounded_double = p_dbl(upper = 1),
+#'   double_with_trafo = p_dbl(-1, 1, trafo = exp),
+#'   extra_double = p_dbl(0, 1, special_vals = "xxx", tags = "tagged"),
+#'   factor_param = p_fct(c("a", "b", "c")),
+#'   factor_param_with_implicit_trafo = p_fct(list(a = 1, b = 2, c = list()))
+#' )
+#' print(params)
+#'
+#' params$trafo(list(
+#'   bounded_double = 1,
+#'   double_with_trafo = 1,
+#'   factor_param = "c",
+#'   factor_param_with_implicit_trafo = "c"
+#' ))
+#' @family ParamSet construction helpers
+#' @name Domain
+NULL
 
+#' @rdname Domain
 #' @export
-p_int = p_dbl = p_fct = p_lgl = p_uty = p_lgl = function(..., requires = NULL, trafo = NULL) {
+p_int = function(...) {
+  domain(constructor = ParamInt, ...)
+}
+
+#' @rdname Domain
+#' @export
+p_dbl = function(...) {
+  domain(constructor = ParamDbl, ...)
+}
+
+#' @rdname Domain
+#' @export
+p_uty = function(...) {
+  domain(constructor = ParamUty, ...)
+}
+
+#' @rdname Domain
+#' @export
+p_lgl = function(...) {
+  domain(constructor = ParamLgl, ...)
+}
+
+#' @rdname Domain
+#' @export
+p_fct = function(..., requires = NULL, trafo = NULL) {
+  extracted = (function(id, levels, ...) {
+    list(levels = levels, rest = list(...))
+  })(id = "ID", ...)
+  levels = extracted$levels
+  if (!is.character(levels)) {
+    # if the "levels" argument is not a character vector, then
+    # we add a trafo.
+    assert(check_atomic_vector(levels), check_list(levels))
+    if (is.null(names(levels))) {
+      names(levels) = as.character(levels)
+    }
+    trafo = crate(function(x) {
+      x = levels[[x]]
+      if (!is.null(trafo)) x = trafo(x)
+      x
+    }, trafo, levels)
+    constargs = extracted$rest
+    constargs$levels = names(levels)
+    invoke(domain, requires = requires, trafo = trafo, .args = constargs)
+  } else {
+    domain(constructor = ParamFct, ...)
+  }
+}
+
+# Construct the actual `Domain` object
+# @param Constructor: The ParamXxx to call `$new()` for.
+domain = function(constructor, ..., requires = NULL, trafo = NULL) {
   constargs = list(...)
   if ("id" %in% names(constargs)) stop("id must not be given to p_xxx")
-  constructor = switch(as.character(sys.call()[[1]]),
-    p_int = ParamInt,
-    p_dbl = ParamDbl,
-    p_uty = ParamUty,
-    p_lgl = ParamLgl,
-    p_fct = {
-      extracted = (function(id, levels, ...) {
-        list(levels = levels, rest = list(...))
-      })(id = "ID", ...)
-      levels = extracted$levels
-      if (!is.character(levels)) {
-        assert(check_atomic_vector(levels), check_list(levels))
-        if (is.null(names(levels))) {
-          names(levels) = as.character(levels)
-        }
-        trafo = crate(function(x) {
-          x = levels[[x]]
-          if (!is.null(trafo)) x = trafo(x)
-          x
-        }, trafo, levels)
-        constargs = extracted$rest
-        constargs$levels = names(levels)
-      }
-      ParamFct
-    },
-    stop("Function must not be renamed.")
-  )
 
-  # check that this doesn't error
-  invoke(constructor$new, id = "ID", .args = constargs)
+  # check that `...` are valid by constructing and making sure this doesn't error
+  # The object generated here is thrown away, this is only for checks.
+  param = invoke(constructor$new, id = "ID", .args = constargs)
 
+  # requires may be an expression, but may also be quote() or expression()
   requires_expr = substitute(requires)
   if (length(requires_expr) == 1 ||
       isTRUE(as.character(requires_expr[[1]]) %in% c("quote", "expression"))) {
     requires_expr = requires
   }
 
-  structure(list(
-    constructor = constructor,
-    constargs = constargs,
+  # repr: what to print
+  repr = sys.call(-1)
+  traforep = repr$trafo
+
+  repr = as.call(c(as.list(repr)[[1]], constargs))  # use cleaned up constargs
+  repr$requires = requires_expr  # put `requires` at the end, but only if not NULL
+  repr$trafo = traforep  # put `trafo` at the end, but only if not NULL
+
+  set_class(list(
+    param = param,
     trafo = assert_function(trafo, null.ok = TRUE),
-    requirements = parse_requires(requires_expr, environment()),
-    callsymbol = sys.call()[[1]],
-    reqtraforep = discard(as.list(sys.call())[c("trafo", "requires")], is.null)
-  ), class = "Domain")
+    requirements = parse_requires(requires_expr, parent.frame(2)),
+    repr = repr
+  ), "Domain")
 }
 
 #' @export
 print.Domain = function(x, ...) {
-  print(as.call(c(list(x$callsymbol), x$constargs, x$reqtraforep)))
+  print(x$repr)
 }
 
+# Parse the expression for requirements, as they are given to p_int, p_dbl etc.
+# We allow `==`, `%in%` `&&`, and `(`/`)` to occur in such expressions.
+# We construct a list of `Condition` objects with an additional `on` element of what
+# the condition should refer to.
+#
+# @example
+# parse_requires(quote(x == 1 && y %in% c("b", "c")), environment())
+# # same as:
+# list(
+#   list(on = "x", CondEqual$new(1)),
+#   list(on = "y", CondAnyOf$new(c("b", "c")))
+# )
 parse_requires = function(requires_expr, evalenv) {
   if (is.null(requires_expr)) return(NULL)
+
+  # throw(): Give generic helpful error message.
   throw = function(msg = NULL) stopf("Requirement '%s' is broken%s", deparse1(requires_expr), sprintf(":\n%s", msg))
 
   if (!is.language(requires_expr)) throw()
@@ -113,19 +255,26 @@ parse_requires = function(requires_expr, evalenv) {
   symbol_and = as.symbol("&&")
   symbol_in = as.symbol("%in%")
 
+  # unpack_parens(): turn (((x))) into x
   unpack_parens = function(expr) {
     while (is.recursive(expr) && length(expr) && identical(expr[[1]], symbol_paren)) expr = expr[[2]]
     expr
   }
 
+  # recurse_expression: turn an expression into a list of conditions / referents (i.e. `on`)
   recurse_expression = function(cur_expr) {
     cur_expr = unpack_parens(cur_expr)
     constructor = NULL
+
+    # found something unexpected, e.g. a single term when all we should find should be at least a binary operator
     if (!is.recursive(cur_expr) || length(cur_expr) <= 1) throw()
+
+    # recurse on `&&`: combine LHS and RHS
     if (identical(cur_expr[[1]], symbol_and)) {
       return(c(recurse_expression(cur_expr[[2]]), recurse_expression(cur_expr[[3]])))
     }
 
+    # go by case: `==` (CondEqual), `%in%` (CondAnyOf), or error if anything else
     if (identical(cur_expr[[1]], symbol_equal)) {
       constructor = CondEqual
     } else if (identical(cur_expr[[1]], symbol_in)) {
@@ -134,9 +283,20 @@ parse_requires = function(requires_expr, evalenv) {
       throw()
     }
 
+    # get value and referent
     comparand = unpack_parens(cur_expr[[2]])
     value = unpack_parens(cur_expr[[3]])
 
+    # referent must always be on the LHS. This is necessary because otherwise
+    # we would not know which side is a value and which side is a Parameter name.
+    # E.g.
+    # a = 1.0
+    # b = 2.0
+    # ps(a = p_dbl(), b = p_dbl(),
+    #   c = p_int(requires = a == b)
+    # )
+    # would be ambiguous if we did not demand that `a` is the name of the parameter, and
+    # `b` is just the value (2.0).
     if (!is.symbol(comparand)) throw("LHS must be a parameter name")
     comparand = as.character(comparand)
     value = eval(value, envir = evalenv)
@@ -145,4 +305,3 @@ parse_requires = function(requires_expr, evalenv) {
 
   recurse_expression(requires_expr)
 }
-
--- R/tune.R
+++ R/tune.R
@@ -1,4 +1,110 @@
-# Tune token
+#' @include helper.R
+
+#' @title Indicate that a Parameter Value should be Tuned
+#'
+#' @description
+#' `to_tune()` creates a `TuneToken` object which can be assigned to the `$values` slot of a [`ParamSet`] as an
+#' alternative to a concrete value. This indicates that the value is not given directly but should be tuned using
+#' [bbotk][bbotk::bbotk-package] or [mlr3tuning][mlr3tuning::mlr3tuning-package]. If the thus parameterized object
+#' is invoked directly, without being wrapped by or given to a tuner, it will give an error.
+#'
+#' The tuning range [`ParamSet`] that is constructed from the `TuneToken` values in a [`ParamSet`]'s `$values` slot
+#' can be accessed through the `ParamSet$tune_ps` active bindng. This is done automatically by tuners if no tuning range
+#' is given, but it is also possible to access the `$tune_ps` active binding, modify it further, and give the modified
+#' [`ParamSet`] to a tuning function (or do anything else with it, nobody is judging you).
+#'
+#' A `TuneToken` represents the range over which the parameter whose `$values` slot it occupies should be tuned over. It
+#' can be constructed via the `to_tune()` function in one of several ways:
+#'
+#' * **`to_tune()`**: Indicates a parameter should be tuned over its entire range. Only applies to finite parameters
+#'   (i.e. discrete or bounded numeric parameters)
+#' * **`to_tune(lower, upper)`**: Indicates a numeric parameter should be tuned in the inclusive interval spanning
+#'   `lower` to `upper`. Depending on the parameter, integer (if it is a [`ParamInt`]) or real values (if it is a
+#'   [`ParamDbl`]) are used.
+#' * **`to_tune(levels)`**: Indicates a parameter should be tuned through the given discrete values. `levels` can be any
+#'   named or unnamed atomic vector or list (although in the unnamed case it must be possible to construct a
+#'   corresponding `character` vector with distinct values using `as.character`).
+#' * **`to_tune(<Domain>)`**: The given [`Domain`] object (constructed e.g. with [`p_int()`] or [`p_fct()`]) indicates
+#'   the range which should be tuned over. The supplied `trafo` function is used for parameter transformation.
+#' * **`to_tune(<Param>)`**: The given [`Param`] object indicates the range which should be tuned over.
+#' * **`to_tune(<ParamSet>)`**: The given [`ParamSet`] is used to tune over a single `Param`. This is useful for cases
+#'   where a single evaluation-time parameter value (e.g. [`ParamUty`]) is constructed from multiple tuner-visible
+#'   parameters (which may not be `ParamUty`). The supplied [`ParamSet`] should always contain a `$trafo` function,
+#'   which must always return a named `list` with a single entry with the name of the [`Param`] that this `TuneToken`
+#'   object corresponds to.
+#'
+#' The `TuneToken` object's internals are subject to change and should not be relied upon. `TuneToken` objects should
+#' only be constructed via `to_tune()`, and should only be used by giving them to `$values` of a [`ParamSet`].
+#' @param ... if given, restricts the range to be tuning over, as described above.
+#' @return A `TuneToken` object.
+#' @examples
+#' params = ParamSet$new(list(
+#'   ParamInt$new("int", 0, 10),
+#'   ParamInt$new("int_unbounded"),
+#'   ParamDbl$new("dbl", 0, 10),
+#'   ParamDbl$new("dbl_unbounded"),
+#'   ParamFct$new("fct", c("a", "b", "c")),
+#'   ParamUty$new("uty1"),
+#'   ParamUty$new("uty2"),
+#'   ParamUty$new("uty3"),
+#'   ParamUty$new("uty4"),
+#'   ParamUty$new("uty5")
+#' ))
+#'
+#' params$values = list(
+#'
+#'   # tune over entire range of `int`, 0..10:
+#'   int = to_tune(),
+#'
+#'   # tune over 2..7:
+#'   int_unbounded = to_tune(2, 7),
+#'
+#'   # tune on a log scale in range 1..100:
+#'   dbl = to_tune(p_dbl(log(1), log(100), trafo = exp)),
+#'
+#'   # nothing keeps us from tuning a dbl over integer values
+#'   dbl_unbounded = to_tune(p_int(1, 10)),
+#'
+#'   # tune over values "a" and "b" only
+#'   fct = to_tune(c("a", "b")),
+#'
+#'   # tune over integers 2..8.
+#'   # ParamUty needs type information in form of p_xxx() in to_tune.
+#'   uty1 = to_tune(p_int(2, 8)),
+#'
+#'   # tune uty2 like a factor, trying 1, 10, and 100:
+#'   uty2 = to_tune(c(1, 10, 100)),
+#'
+#'   # tune uty3 like a factor. The factor levels are the names of the list
+#'   # ("exp", "square"), but the trafo will generate the values from the list.
+#'   # This way you can tune an objective that has function-valued inputs.
+#'   uty3 = to_tune(list(exp = exp, square = function(x) x^2)),
+#'
+#'   # tune through multiple parameters. When doing this, the ParamSet in tune()
+#'   # must have the trafo that generates a list with one element and the right
+#'   # name:
+#'   uty4 = to_tune(ps(
+#'     base = p_dbl(0, 1),
+#'     exp = p_int(0, 3),
+#'     .extra_trafo = function(x, param_set) {
+#'       list(uty4 = x$base ^ x$exp)
+#'     }
+#'   )),
+#'
+#'   # not all values need to be tuned!
+#'   uty5 = 100
+#' )
+#'
+#' print(params$tune_ps)
+#'
+#' # Change `$values` directly and generate new `tune_ps` to play around
+#' params$values$uty3 = 8
+#' params$values$uty2 = to_tune(c(2, 4, 8))
+#'
+#' print(params$tune_ps)
+#'
+#' @family ParamSet construction helpers
+#' @aliases TuneToken
 #' @export
 to_tune = function(...) {
   call = sys.call()
@@ -6,12 +112,16 @@ to_tune = function(...) {
     stop("to_tune() must have zero, one or two arguments.")
   }
   if (...length() == 2) {
+    # Two arguments: tune over a range
     type = "RangeTuneToken"
     content = (function(lower, upper) {
       list(lower = assert_number(lower), upper = assert_number(upper))}
     )(...)
   } else if (...length() == 1) {
     content = list(...)[[1]]
+    # one argument: tune over an object. that object can be something
+    # that can be converted to a ParamSet (ParamSet itself, Param, or Domain),
+    # otherwise it must be something that can be converted to a ParamFct Domain.
     if (!test_multi_class(content, c("ParamSet", "Param", "Domain"))) {
       assert(
         check_atomic_vector(content, names = "unnamed"),
@@ -23,6 +133,7 @@ to_tune = function(...) {
     }
     type = "ObjectTuneToken"
   } else {
+    # Zero arguments: Tune over whole parameter
     type = "FullTuneToken"
     content = list()
   }
@@ -46,6 +157,12 @@ print.ObjectTuneToken = function(x, ...) {
   print(x$content)
 }
 
+# tunetoken_to_ps: Convert a `TuneToken` to a `ParamSet` that tunes over this.
+# Needs the corresponding `Param` to which the `TuneToken` refers, both to
+# get the range (e.g. if `to_tune()` was used) and to verify that the `TuneToken`
+# does not go out of range.
+#
+# Makes liberal use to `pslike_to_ps` (converting Param, ParamSet, Domain to ParamSet)
 tunetoken_to_ps = function(tt, param) {
   UseMethod("tunetoken_to_ps")
 }
@@ -54,7 +171,7 @@ tunetoken_to_ps.FullTuneToken = function(tt, param) {
   if (!param$is_bounded) {
     stopf("%s must give a range for unbounded parameter %s.", tt$call, param$id)
   }
-  ttcontent_to_ps(param, tt$call, param)
+  pslike_to_ps(param, tt$call, param)
 }
 
 tunetoken_to_ps.RangeTuneToken = function(tt, param) {
@@ -65,36 +182,43 @@ tunetoken_to_ps.RangeTuneToken = function(tt, param) {
     stopf("%s not compatible with param %s", tt$call, param$id)
   }
   content = get_r6_constructor(param$class)$new(id = param$id, lower = tt$content$lower, upper = tt$content$upper)
-  ttcontent_to_ps(content, tt$call, param)
+  pslike_to_ps(content, tt$call, param)
 }
 
 tunetoken_to_ps.ObjectTuneToken = function(tt, param) {
-  ttcontent_to_ps(tt$content, tt$call, param)
+  pslike_to_ps(tt$content, tt$call, param)
 }
 
-ttcontent_to_ps = function(content, call, param) {
-  UseMethod("ttcontent_to_ps")
+# Convert something that is `ParamSet`-like (ParamSet, Param, or Domain) to a `ParamSet`.
+# * content is ParamSet --> verify that it is compatible with given `Param`
+# * content is Param --> Wrap in ParamSet
+# * content is Domain --> Wrap in ParamSet, using ps()
+# @param pslike: thing to convert
+# @param call: to_tune()-call, for better debug message
+# @param param: `Param`, that the `pslike` refers to, and therefore needs to be compatible to
+pslike_to_ps = function(pslike, call, param) {
+  UseMethod("pslike_to_ps")
 }
 
-ttcontent_to_ps.Domain = function(content, call, param) {
-  content = do.call(ps, set_names(list(content), param$id))
-  ttcontent_to_ps(content, call, param)
+pslike_to_ps.Domain = function(pslike, call, param) {
+  pslike = do.call(ps, set_names(list(pslike), param$id))
+  pslike_to_ps(pslike, call, param)
 }
 
-ttcontent_to_ps.Param = function(content, call, param) {
-  content = content$clone(deep = TRUE)
-  content$id = param$id
-  content = ParamSet$new(list(content))
-  ttcontent_to_ps(content, call, param)
+pslike_to_ps.Param = function(pslike, call, param) {
+  pslike = pslike$clone(deep = TRUE)
+  pslike$id = param$id
+  pslike = ParamSet$new(list(pslike))
+  pslike_to_ps(pslike, call, param)
 }
 
-ttcontent_to_ps.ParamSet = function(content, call, param) {
-  testpoints = generate_design_grid(content, 2)$transpose()
+pslike_to_ps.ParamSet = function(pslike, call, param) {
+  testpoints = generate_design_grid(pslike, 2)$transpose()
   if (!all(map_lgl(testpoints, function(x) {
     identical(names(x), param$id) && param$test(x[[1]])
   }))) {
     stopf("%s not compatible with param %s", call, param$id)
   }
-  content$set_id = ""
-  content
+  pslike$set_id = ""
+  pslike
 }
