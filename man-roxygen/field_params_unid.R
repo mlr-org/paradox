30fc6ddd10f4a2a2ed5375a6c983149b71d51996
mb706 <mb706@users.noreply.github.com>
2021-01-26 14:54:29 +0100

    adding params_unid (#333)
    * adding params_unid
    
    * document
    
    * small bugfix

---
+++ man-roxygen/field_params_unid.R
@@ -0,0 +1,5 @@
+#' @field params_unid (named `list()`)\cr
+#' List of [Param], named with their true ID. However,
+#' this field has the [Param]'s `$id` value set to a
+#' potentially invalid value. This active binding should
+#' only be used internally.
