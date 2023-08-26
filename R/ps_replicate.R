e9d37e949a13aa5023d9e12d9d2053d2dbc21b9a
mb706 <mlr.developer@mb706.com>
2023-08-26 12:53:49 +0200

    vignette builds

--- R/ps_replicate.R
+++ R/ps_replicate.R
@@ -1,4 +1,4 @@
-
+#' @export
 ps_replicate = function(set, times = length(prefixes), prefixes = sprintf("rep%s", seq_len(times)), tag_sets = FALSE, tag_params = FALSE) {
   assert_count(times)
   assert_character(prefixes, any.missing = FALSE, unique = TRUE, len = times)
--- R/ps_union.R
+++ R/ps_union.R
@@ -1,11 +1,14 @@
-# Create a ParamSet from a list of ParamSets
-# This emulates `ParamSetCollection$new(sets)`, except that
-# - The result is a `ParamSet`, not a `ParamSetCollection`
-# - The ParamSets are allowed to have `$trafo`, which are collected together into a single function.
-# This emulates `ParamSetCollection$new(sets)`, which in particular means that the resulting ParamSet has all the Params
-# from the input `sets`, but some `$id`s are changed: If the ParamSet has a non-empty `set_id`, then the Params will
-# have their <id> changed to <set_id>.<id>. This is also reflected in deps and in `$trafo`.
-# @param sets: list of ParamSet
+#' @title Create a ParamSet from a list of ParamSets
+#'
+#' @description
+#' This emulates `ParamSetCollection$new(sets)`, except that
+#' - The result is a `ParamSet`, not a `ParamSetCollection`
+#' - The ParamSets are allowed to have `$trafo`, which are collected together into a single function.
+#' This emulates `ParamSetCollection$new(sets)`, which in particular means that the resulting ParamSet has all the Params
+#' from the input `sets`, but some `$id`s are changed: If the ParamSet has a non-empty `set_id`, then the Params will
+#' have their <id> changed to <set_id>.<id>. This is also reflected in deps and in `$trafo`.
+#' @param sets: list of ParamSet
+#' @export
 ps_union = function(sets, tag_sets = FALSE, tag_params = FALSE) {
   assert_list(sets, types = "ParamSet")
   if (!length(sets)) return(ParamSet$new())
